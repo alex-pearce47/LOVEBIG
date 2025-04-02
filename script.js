ab5a804ce3ffdc952ed1da6baedff9889703d335
alex-pearce47 <alexandbrandi47@gmail.com>
2025-04-02 00:18:15 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const messages = [
+  "You are blooming beautifully.",
+  "Your smile lights up the world.",
+  "You are exactly where you need to be.",
+  "Kindness is your superpower!",
+  "You are loved, always."
+];
+
+function revealMessage(element) {
+  const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+  element.innerText = randomMessage;
+  element.style.fontSize = "1.2em";
+}
+
+function getRandomQuote() {
+  const quote = messages[Math.floor(Math.random() * messages.length)];
+  document.getElementById("snail-message").innerText = quote;
+}
